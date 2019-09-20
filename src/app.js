6914378fff7a21abbc4b269688234ad18a1073a6
Juliette Abeyta <jabeyta@fluentstream.com>
2019-09-20 09:52:03 -0600

    Refactor form update/sumbit for search form

--- src/app.js
+++ src/app.js
@@ -7,29 +7,49 @@ const client = new ApolloClient({
     uri: "https://api.spacex.land/graphql",
 })
 class App extends React.Component {
-
   state = {
-    missionname: "",    
-    rocketname: "",   
-    launchyear: ""   
+    missionname: "",
+    rocketname: "",
+    launchyear: "",
+    searchResults: ""
+  };
+
+  componentDidMount(){
+    //Let's initially get launch results
+    this.submit();
   }
 
-  search(){
-    let missionname = document.getElementById("missionname").value;
-    let rocketname = document.getElementById("rocketname").value
-    let launchyear = document.getElementById("launchyear").value
+  submit(clear) {
+    let { missionname, rocketname, launchyear } = this.state;
 
-    this.setState({missionname, rocketname, launchyear});
-  }
-  clear(clear){
-    document.getElementById("missionname").value = "";
-    document.getElementById("rocketname").value = ""
-    document.getElementById("launchyear").value = ""
-    this.setState({missionname: "", rocketname: "", launchyear: ""});
+    // If the user wants to clear their search, let's handle that here
+    if (clear){
+      missionname = "";
+      rocketname = "";
+      launchyear = "";
+      this.setState({ missionname, rocketname, launchyear });
+    }
+
+    const searchResults = (
+      <ApolloProvider client={client}>
+        <div className="container">
+          <Missions
+            missionname={missionname}
+            rocketname={rocketname}
+            launchyear={launchyear}
+          />
+        </div>
+      </ApolloProvider>
+    );
+    this.setState({searchResults});
   }
 
-  render(){
-    const {missionname, rocketname, launchyear} = this.state;
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  render() {
+    const { missionname, rocketname, launchyear, searchResults } = this.state;
     return (
       <div className="app-container">
         <h1>Search SpaceX Launches By: </h1>
@@ -39,9 +59,13 @@ class App extends React.Component {
           </label>
           <input
             className="field"
-            onKeyDown={(e)=>{if(e.keyCode === 13){
-              this.search();
-            }}}
+            onChange={(e)=>this.handleChange(e)}
+            onKeyDown={e => {
+              if (e.keyCode === 13) {
+                this.submit();
+              }
+            }}
+            value={missionname}
             type="text"
             name="missionname"
             placeholder="COTS 1"
@@ -52,9 +76,13 @@ class App extends React.Component {
           </label>
           <input
             className="field"
-            onKeyDown={(e)=>{if(e.keyCode === 13){
-              this.search();
-            }}}
+            onChange={(e)=>this.handleChange(e)}
+            onKeyDown={e => {
+              if (e.keyCode === 13) {
+                this.submit();
+              }
+            }}
+            value={rocketname}
             type="text"
             name="rocketname"
             placeholder="Falcon 9"
@@ -65,19 +93,23 @@ class App extends React.Component {
           </label>
           <input
             className="field"
-            onKeyDown={(e)=>{if(e.keyCode === 13){
-              this.search();
-            }}}
+            onChange={(e)=>this.handleChange(e)}
+            onKeyDown={e => {
+              if (e.keyCode === 13) {
+                this.submit();
+              }
+            }}
+            value={launchyear}
             type="text"
             name="launchyear"
-            placeholder="2012"
+            placeholder="2010"
             id="launchyear"
           />
           <button
             className="button"
             type="submit"
             id="submitbutton"
-            onClick={() => this.search()}
+            onClick={() => this.submit()}
           >
             search launches
           </button>
@@ -85,18 +117,15 @@ class App extends React.Component {
             className="button"
             type="submit"
             id="clearbutton"
-            onClick={() => this.clear()}
+            onClick={() => this.submit(true)}
           >
             clear searches
           </button>
         </div>
-        <ApolloProvider client={client}>
-          <div className="container">
-            <Missions missionname={missionname} rocketname={rocketname} launchyear={launchyear} />
-          </div>
-        </ApolloProvider>
+        {searchResults}
       </div>
-    );}
+    );
+  }
 }
 
 export default App;
